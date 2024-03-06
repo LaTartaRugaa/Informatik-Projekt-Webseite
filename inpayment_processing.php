f283235441c92e918e2567b32388fcdd0de6d5e2
LaTartaRugaa <michael@enderli.net>
2024-03-06 18:31:29 +0100

    If-else-Verzweigung zur Überprüfung des Bilduploads wurde erstellt.$

--- inpayment_processing.php
+++ inpayment_processing.php
@@ -9,23 +9,25 @@ if ($conn->connect_errno) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$image = $_FILES["image"];
-$blob_data = file_get_contents($image["tmp_name"]);
-
 $amount = $_POST["amount"];
 $prename = $_POST["prename"];
 $surname = $_POST["surname"];
 
-$sql = "INSERT INTO inpayment (amount, prename, surname, image) VALUES (?, ?, ?, ?)";
-$stmt = $conn -> prepare($sql);
-$stmt->bind_param("ssss", $amount, $prename, $surname, $blob_data);
+// Überprüfen, ob $_Files leer ist (Error 4: No file was uploaded)
+if ($_FILES["image"]["error"] == 4) {
+    $stmt = $conn->prepare("INSERT INTO inpayment (amount, prename, surname) VALUES (?, ?, ?)");
+    $stmt->bind_param("sss", $amount, $prename, $surname);
+} else {
+    $stmt = $conn->prepare("INSERT INTO inpayment (amount, prename, surname, image) VALUES (?, ?, ?, ?)");
+    $blob_data = file_get_contents($_FILES["image"]["tmp_name"]);
+    $stmt->bind_param("ssss", $amount, $prename, $surname, $blob_data);
+}
 
 if ($stmt->execute()) {
     echo "Successfully created new entry";
 } else {
     echo "Error occured: " . $stmt->error;
 }
-
 $stmt->close();
 
 $conn->close();
