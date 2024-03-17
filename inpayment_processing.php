746e7c12f16bb378d38f2902690a8af3a966da2a
LaTartaRugaa <michael@enderli.net>
2024-03-17 13:55:13 +0100

    Nach Absenden des ausgefüllten Formulars wird man nun autmatisch wieder zur subpage_2.php zurückgeleitet.

--- inpayment_processing.php
+++ inpayment_processing.php
@@ -1,41 +1,42 @@
 <?php
 
-$user = "root";
-$password = "root";
-$host = "localhost";
-$db_name = "donations";
+    $user = "root";
+    $password = "root";
+    $host = "localhost";
+    $db_name = "donations";
 
-$conn = new mysqli($host, $user, $password, $db_name);
-if ($conn->connect_errno) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-$amount = $_POST["amount"];
-$prename = $_POST["prename"];
-$surname = $_POST["surname"];
-$email = $_POST["email"];
-
-// Überprüfen, ob $_FILES leer ist (Error 4: No file was uploaded)
-if ($_FILES["image"]["error"] == 4) {
-    $stmt = $conn->prepare("INSERT INTO inpayment (amount, prename, surname, email) VALUES (?, ?, ?, ?)");
-    $stmt->bind_param("ssss", $amount, $prename, $surname, $email);
-} else {
-    $stmt = $conn->prepare("INSERT INTO inpayment (amount, prename, surname, email, image) VALUES (?, ?, ?, ?, ?)");
-    if ($_FILES["image"]["size"] <= 1048576) {
-        $blob_data = file_get_contents($_FILES["image"]["tmp_name"]);
-        $stmt->bind_param("sssss", $amount, $prename, $surname, $email, $blob_data);
-    } else {
-        die("Max. file size is 1Mb");
+    $conn = new mysqli($host, $user, $password, $db_name);
+    if ($conn->connect_errno) {
+        die("Connection failed: " . $conn->connect_error);
     }
-}
 
-if ($stmt->execute()) {
-    echo "Successfully created new entry";
-} else {
-    echo "Error occured: " . $stmt->error;
-}
-$stmt->close();
+    $amount = $_POST["amount"];
+    $prename = $_POST["prename"];
+    $surname = $_POST["surname"];
+    $email = $_POST["email"];
 
-$conn->close();
+    // Überprüfen, ob $_FILES leer ist (Error 4: No file was uploaded)
+    if ($_FILES["image"]["error"] == 4) {
+        $stmt = $conn->prepare("INSERT INTO inpayment (amount, prename, surname, email) VALUES (?, ?, ?, ?)");
+        $stmt->bind_param("ssss", $amount, $prename, $surname, $email);
+    } else {
+        $stmt = $conn->prepare("INSERT INTO inpayment (amount, prename, surname, email, image) VALUES (?, ?, ?, ?, ?)");
+        if ($_FILES["image"]["size"] <= 1048576) {
+            $blob_data = file_get_contents($_FILES["image"]["tmp_name"]);
+            $stmt->bind_param("sssss", $amount, $prename, $surname, $email, $blob_data);
+        } else {
+            die("Max. file size is 1Mb");
+        }
+    }
+
+    if ($stmt->execute()) {
+        echo "Successfully created new entry";
+    } else {
+        echo "Error occured: " . $stmt->error;
+    }
+    $stmt->close();
+    $conn->close();
+
+    header("Refresh: 1; URL=subpage_2.php");
 
 ?>
